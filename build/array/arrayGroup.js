5b94133bd0b374735d023920f9123ba68923c17c
cpagejs <3205214720@163.com>
2022-09-14 23:46:08 +0800

    arrayGroup优化

--- build/array/arrayGroup.js
+++ build/array/arrayGroup.js
@@ -1,5 +1,6 @@
 ﻿"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var type_1 = require("../type/type");
 /**
  * 把数组均分成几等份，并返回一个新的数组
  * @param {Array<any>} arr 数组
@@ -9,6 +10,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function arrayGroup(arr, num) {
     if (!arr || !arr.length) {
         return [];
+    }
+    if (type_1.default(num) != "number") {
+        console.error('project-libs（arrayGroup方法参数错误）：num的必须是数字');
+    }
+    if (num < 0) {
+        console.error('project-libs（arrayGroup方法参数错误）：num必须大于0');
     }
     var index = 0;
     var newArray = [];
--- src/array/arrayGroup.ts
+++ src/array/arrayGroup.ts
@@ -1,3 +1,5 @@
+import type from "../type/type";
+
 /**
  * 把数组均分成几等份，并返回一个新的数组
  * @param {Array<any>} arr 数组
@@ -6,8 +8,17 @@
  */
 export default function arrayGroup(arr: Array<any>, num: number): Array<any> {
   if(!arr || !arr.length){
-    return []
+    return [];
   }
+
+  if(type(num) != "number") {
+    console.error('project-libs（arrayGroup方法参数错误）：num的必须是数字');
+  }
+
+  if(num < 0) {
+    console.error('project-libs（arrayGroup方法参数错误）：num必须大于0');
+  }
+
   let index = 0;
   let newArray = [];
   while(index < arr.length) {
