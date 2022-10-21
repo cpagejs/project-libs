8fc84fb2c7bfa7b9ecdc537a90ccf63864401674
cpagejs <3205214720@163.com>
2022-10-21 19:40:13 +0800

    doc

---
+++ test/time.test.ts
@@ -0,0 +1,10 @@
+import 'mocha';
+import { expect } from 'chai';
+import timeBeauty from '../src/time/timeBeauty';
+
+describe('time时间函数', function(){
+  it('timeBeauty', function() {
+    expect(timeBeauty('1666351987000')).to.eql('2分钟前');
+  });
+
+});
