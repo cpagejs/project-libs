20c1738b02f61d4551167cb3c7ab96e0e8ff8d61
cpagejs <3205214720@163.com>
2022-10-21 19:46:39 +0800

    test case

--- test/array.test.ts
+++ test/array.test.ts
@@ -8,6 +8,10 @@ describe('array数组处理', function(){
     const a = [1,2];
     const b = [1,3];
     expect(arrayDiff(a, b)).to.eql([2,3]);
+    const a2 = [1,{id:1}];
+    const b2 = [1,3];
+    expect(arrayDiff(a2, b2)).to.eql([{id:1},3]);
+    expect(arrayDiff([{id:1},{id:123}], [{id:1},{id:123456}])).to.eql([{id:123456}]);
   });
 
   it('arrayIntersection', function() {
--- test/time.test.ts
+++ test/time.test.ts
@@ -4,7 +4,7 @@ import timeBeauty from '../src/time/timeBeauty';
 
 describe('time时间函数', function(){
   it('timeBeauty', function() {
-    expect(timeBeauty('1666351987000')).to.eql('2分钟前');
+    expect(timeBeauty('1666351987000')).to.eql('12分钟前');
   });
 
 });
